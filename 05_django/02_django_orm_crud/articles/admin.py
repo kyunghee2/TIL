2bda7af86039c99c7760fe2c144c4ca57be9548f
kyunghee2 <kyungh103@gmail.com>
2019-10-30 14:04:06 +0900

    관리자 사이트 커스터마이징

--- 05_django/02_django_orm_crud/articles/admin.py
+++ 05_django/02_django_orm_crud/articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('pk','title','content','created_at','updated_at',)
+    list_display_links = ('title',) #링크설정
+    list_filter = ('created_at',) #필터설정
+    #list_editable = ('content',) #리스트에서 수정모드 설정
+    #list_per_page = 2 #한페이지에 리스트 개수
+
+admin.site.register(Article, ArticleAdmin)

8a17253e0bc63b2b6781f39df6f87c8b350f2b85
kyunghee2 <kyungh103@gmail.com>
2019-11-05 12:43:03 +0900

    movie 사이트 코드 리팩토링

--- 05_django/10_django_movie_project/movies/models.py
+++ 05_django/10_django_movie_project/movies/models.py
@@ -15,4 +15,18 @@ class Movie(models.Model):
     #객체 표시 형식 수정
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+
+class Comment(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    content = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #Model Level에서 Metadata 설정
+    class Meta:
+        ordering = ['-pk',] #정렬설정
+
+    def __str__(self):
+        return self.content
        

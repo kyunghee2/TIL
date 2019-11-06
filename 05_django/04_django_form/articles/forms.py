8a2d4269804e59885620f0d0a7be026b41899a75
kyunghee2 <kyungh103@gmail.com>
2019-11-06 17:50:19 +0900

    django form model방식으로 변경

--- 05_django/04_django_form/articles/forms.py
+++ 05_django/04_django_form/articles/forms.py
@@ -1,26 +1,59 @@
 from django import forms
-
-class ArticleForm(forms.Form):
+from .models import Article
+# class ArticleForm(forms.Form):
+#     title = forms.CharField(
+#         max_length=30,
+#         # HTML Tag와 동일
+#         label='제목',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'class':'title',
+#                 'placeholder':'제목을 입력해 주세요...!',
+#             }
+#         )
+#     )
+#     content = forms.CharField(
+#         label='내용',
+#         # widget : Input Type 지정 -> Textarea / 알맞은 속성값 부여
+#         widget=forms.Textarea(
+#             attrs={
+#                 'class':'content',
+#                 'placeholder':'내용을 입력해 주세요오오오옹',
+#                 'rows':5,
+#                 'cols':30,
+#             }
+#         )
+#     )
+# ModelForm
+# 1. ModelForm 클래스를 상속받아 사용한다
+# 2. 메타데이터로 Model정보를 건네주면, ModelForm이 자동으로 field에 맞춰 input을 생성해준다
+class ArticleForm(forms.ModelForm):
+    #메타데이터 :  데이터의 데이터
+    #ex) 사진 한장 (촬영장비이름, 촬영환경 등)
     title = forms.CharField(
-        max_length=30,
-        # HTML Tag와 동일
         label='제목',
+        max_length=10,
         widget=forms.TextInput(
             attrs={
                 'class':'title',
-                'placeholder':'제목을 입력해 주세요...!',
+                'placeholder':'제목입력'
             }
         )
     )
     content = forms.CharField(
         label='내용',
-        # widget : Input Type 지정 -> Textarea / 알맞은 속성값 부여
+        max_length=10,
         widget=forms.Textarea(
             attrs={
                 'class':'content',
-                'placeholder':'내용을 입력해 주세요오오오옹',
+                'placeholder':'내용입력',
                 'rows':5,
-                'cols':30,
+                'cols':30
             }
         )
     )
+    class Meta:
+        model = Article
+        fields = '__all__' #전체가져오기
+        #fields = ('title','content',) #선택해서 데이터 가져오기
+

0ef32d7c6a1b9bc0e25461ecd8165272dfc0427d
kyunghee2 <kyungh103@gmail.com>
2019-11-12 16:31:34 +0900

    model relationship - many to many

---
+++ 05_django/05_model_relation/manytomany/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Doctor(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.pk}번 의사 {self.name}'
+
+class Patient(models.Model):
+    name = models.TextField()
+    #doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    #doctors = models.ManyToManyField(Doctor, through='Reservation')
+    #참조로 조회할 수 있게 설정 through=...
+    #역참조로 조회할 수 있게 설정 related_name=...
+    #doctors = models.ManyToManyField(Doctor, through='Reservation', related_name="patients")
+    doctors = models.ManyToManyField(Doctor, related_name="patients")
+
+    def __str__(self):
+        return f'{self.pk}번 환자 {self.name}'
+
+# 중개모델
+# class Reservation(models.Model):
+#     doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return f'{self.doctor.id}의사의 {self.patient.id}번 환자'
+    
+    
